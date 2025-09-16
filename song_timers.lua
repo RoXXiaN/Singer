b46d63c29a0f914363dba8873d3825ffc99a431d
RoXXiaN <helder.m@hotmail.de>
2025-09-16 21:59:55 +0200

    Update song_timers.lua

--- song_timers.lua
+++ song_timers.lua
@@ -34,96 +34,96 @@ song_debuffs = {
     }
 
 local equip_mods = {
-    [18342] = {0.2},            -- 'Gjallarhorn',    -- 75
-    [18577] = {0.2},            -- 'Gjallarhorn',    -- 80
-    [18578] = {0.2},            -- 'Gjallarhorn',    -- 85
-    [18579] = {0.3},            -- 'Gjallarhorn',    -- 90
-    [18580] = {0.3},            -- 'Gjallarhorn',    -- 95
-    [18572] = {0.4},            -- 'Gjallarhorn',    -- 99
-    [18840] = {0.4},            -- 'Gjallarhorn',    -- 99-2
-    [18575] = {0.25},           -- 'Daurdabla',      -- 90
-    [18576] = {0.25},           -- 'Daurdabla',      -- 95
-    [18571] = {0.3},            -- 'Daurdabla',      -- 99
-    [18839] = {0.3},            -- 'Daurdabla',      -- 99-2
-    [19000] = {0.1},            -- 'Carnwenhan',     -- 75
-    [19069] = {0.2},            -- 'Carnwenhan',     -- 80
-    [19089] = {0.3},            -- 'Carnwenhan',     -- 85
-    [19621] = {0.4},            -- 'Carnwenhan',     -- 90
-    [19719] = {0.4},            -- 'Carnwenhan',     -- 95
-    [19828] = {0.5},            -- 'Carnwenhan',     -- 99
-    [19957] = {0.5},            -- 'Carnwenhan',     -- 99-2
-    [20561] = {0.5},            -- 'Carnwenhan',     -- 119
-    [20562] = {0.5},            -- 'Carnwenhan',     -- 119-2
-    [20586] = {0.5},            -- 'Carnwenhan',     -- 119-3
-    [21398] = {0.5},            -- 'Marsyas',
-    [21400] = {0.1},            -- 'Blurred Harp',
-    [21401] = {0.2,Ballad=0.2}, -- 'Blurred Harp +1',
-    [21405] = {0.2} ,           -- 'Eminent Flute',
-    [21404] = {0.3},			-- 'Linos'			-- assumes +2 songs augment
+	[18342] = {0.2},            -- 'Gjallarhorn',    -- 75
+	[18577] = {0.2},            -- 'Gjallarhorn',    -- 80
+	[18578] = {0.2},            -- 'Gjallarhorn',    -- 85
+	[18579] = {0.3},            -- 'Gjallarhorn',    -- 90
+	[18580] = {0.3},            -- 'Gjallarhorn',    -- 95
+	[18572] = {0.4},            -- 'Gjallarhorn',    -- 99
+	[18840] = {0.4},            -- 'Gjallarhorn',    -- 99-2
+	[18575] = {0.25},           -- 'Daurdabla',      -- 90
+	[18576] = {0.25},           -- 'Daurdabla',      -- 95
+	[18571] = {0.3},            -- 'Daurdabla',      -- 99
+	[18839] = {0.3},            -- 'Daurdabla',      -- 99-2
+	[19000] = {0.1},            -- 'Carnwenhan',     -- 75
+	[19069] = {0.2},            -- 'Carnwenhan',     -- 80
+	[19089] = {0.3},            -- 'Carnwenhan',     -- 85
+	[19621] = {0.4},            -- 'Carnwenhan',     -- 90
+	[19719] = {0.4},            -- 'Carnwenhan',     -- 95
+	[19828] = {0.5},            -- 'Carnwenhan',     -- 99
+	[19957] = {0.5},            -- 'Carnwenhan',     -- 99-2
+	[20561] = {0.5},            -- 'Carnwenhan',     -- 119
+	[20562] = {0.5},            -- 'Carnwenhan',     -- 119-2
+	[20586] = {0.5},            -- 'Carnwenhan',     -- 119-3
+	[21398] = {0.5},            -- 'Marsyas',
+	[21400] = {0.1},            -- 'Blurred Harp',
+	[21401] = {0.2,Ballad=0.2}, -- 'Blurred Harp +1',
+	[21405] = {0.2} ,           -- 'Eminent Flute',
+	[21404] = {0.3},			-- 'Linos'			-- assumes +2 songs augment
 	[22305] = {0.2},			-- 'Loughnashade I',
 	[22306] = {0.3},			-- 'Loughnashade II',
 	[22307] = {0.4},			-- 'Loughnashade III',
-    [20629] = {0.05},           -- 'Legato Dagger',
-    [20599] = {0.05},           -- 'Kali',
-    [27672] = {Paeon=0.1},      -- 'Brioso Roundlet',
-    [27693] = {Paeon=0.1},      -- 'Brioso Roundlet +1',
-    [23049] = {Paeon=0.1},      -- 'Brioso Roundlet +2',
-    [23384] = {Paeon=0.2},      -- 'Brioso Roundlet +3',
+	[20629] = {0.05},           -- 'Legato Dagger',
+	[20599] = {0.05},           -- 'Kali',
+	[27672] = {Paeon=0.1},      -- 'Brioso Roundlet',
+	[27693] = {Paeon=0.1},      -- 'Brioso Roundlet +1',
+	[23049] = {Paeon=0.1},      -- 'Brioso Roundlet +2',
+	[23384] = {Paeon=0.2},      -- 'Brioso Roundlet +3',
 	[23904] = {Paeon=0.2},      -- 'Brioso Roundlet +4',
 	[27952] = {Lullaby=0.1},	-- 'Brioso Cuffs',
 	[27973] = {Lullaby=0.1},	-- 'Brioso Cuffs +1',
 	[23183] = {Lullaby=0.1},	-- 'Brioso Cuffs +2',
 	[23518] = {Lullaby=0.2},	-- 'Brioso Cuffs +3',
 	[23994] = {Lullaby=0.2},	-- 'Brioso Cuffs +4',
-    [28074] = {0.1},            -- 'Mdk. Shalwar +1',
-    [25865] = {0.12},           -- 'Inyanga Shalwar',
-    [25866] = {0.15},           -- 'Inyanga Shalwar +1',
-    [25882] = {0.17},           -- 'Inyanga Shalwar +2',
-    [28232] = {0.1},            -- 'Brioso Slippers',
-    [28253] = {0.11},           -- 'Brioso Slippers +1',
-    [23317] = {0.13},           -- 'Brioso Slippers +2',
-    [23652] = {0.15},           -- 'Brioso Slippers +3',
+	[28074] = {0.1},            -- 'Mdk. Shalwar +1',
+	[25865] = {0.12},           -- 'Inyanga Shalwar',
+	[25866] = {0.15},           -- 'Inyanga Shalwar +1',
+	[25882] = {0.17},           -- 'Inyanga Shalwar +2',
+	[28232] = {0.1},            -- 'Brioso Slippers',
+	[28253] = {0.11},           -- 'Brioso Slippers +1',
+	[23317] = {0.13},           -- 'Brioso Slippers +2',
+	[23652] = {0.15},           -- 'Brioso Slippers +3',
 	[24084] = {0.15},           -- 'Brioso Slippers +4',
-    [11073] = {Madrigal=0.1},   -- 'Aoidos\' Calot +2',
-    [11093] = {0.1,Minuet=0.1}, -- 'Aoidos\' Hngrln. +2',
-    [11113] = {March=0.1},      -- 'Ad. Mnchtte. +2',
-    [11133] = {Ballad=0.1},     -- 'Aoidos\' Rhing. +2',
-    [11153] = {Scherzo=0.1},    -- 'Aoidos\' Cothrn. +2',
-    [11618] = {0.1},            -- 'Aoidos\' Matinee',
-    [26031] = {0.1},            -- 'Brioso Whistle',
-    [26032] = {0.2},            -- 'Moonbow Whistle',
-    [26033] = {0.3},            -- 'Mnbw. Whistle +1',
-    [26758] = {Madrigal=0.1},   -- 'Fili Calot',
-    [26759] = {Madrigal=0.1},   -- 'Fili Calot +1',
+	[11073] = {Madrigal=0.1},   -- 'Aoidos\' Calot +2',
+	[11093] = {0.1,Minuet=0.1}, -- 'Aoidos\' Hngrln. +2',
+	[11113] = {March=0.1},      -- 'Ad. Mnchtte. +2',
+	[11133] = {Ballad=0.1},     -- 'Aoidos\' Rhing. +2',
+	[11153] = {Scherzo=0.1},    -- 'Aoidos\' Cothrn. +2',
+	[11618] = {0.1},            -- 'Aoidos\' Matinee',
+	[26031] = {0.1},            -- 'Brioso Whistle',
+	[26032] = {0.2},            -- 'Moonbow Whistle',
+	[26033] = {0.3},            -- 'Mnbw. Whistle +1',
+	[26758] = {Madrigal=0.1},   -- 'Fili Calot',
+	[26759] = {Madrigal=0.1},   -- 'Fili Calot +1',
 	[23094] = {Madrigal=0.1},   -- 'Fili Calot +2',
 	[23429] = {Madrigal=0.1},   -- 'Fili Calot +3',
-    [26916] = {0.11,Minuet=0.1},-- 'Fili Hongreline',
-    [26917] = {0.12,Minuet=0.1},-- 'Fili Hongreline +1',
+	[26916] = {0.11,Minuet=0.1},-- 'Fili Hongreline',
+	[26917] = {0.12,Minuet=0.1},-- 'Fili Hongreline +1',
 	[23161] = {0.13,Minuet=0.1},-- 'Fili Hongreline +2',
 	[23496] = {0.14,Minuet=0.1},-- 'Fili Hongreline +3',
-    [27070] = {March=0.1},      -- 'Fili Manchettes',
-    [27071] = {March=0.1},      -- 'Fili Manchettes +1',
+	[27070] = {March=0.1},      -- 'Fili Manchettes',
+	[27071] = {March=0.1},      -- 'Fili Manchettes +1',
 	[23228] = {March=0.1},      -- 'Fili Manchettes +2',
 	[23563] = {March=0.1},      -- 'Fili Manchettes +3',
-    [27255] = {Ballad=0.1},     -- 'Fili Rhingrave',
-    [27256] = {Ballad=0.1},     -- 'Fili Rhingrave +1',
+	[27255] = {Ballad=0.1},     -- 'Fili Rhingrave',
+	[27256] = {Ballad=0.1},     -- 'Fili Rhingrave +1',
 	[23295] = {Ballad=0.1},     -- 'Fili Rhingrave +2',
 	[23630] = {Ballad=0.1},     -- 'Fili Rhingrave +3',
-    [27429] = {Scherzo=0.1},    -- 'Fili Cothurnes',
-    [27430] = {Scherzo=0.1},    -- 'Fili Cothurnes +1',
+	[27429] = {Scherzo=0.1},    -- 'Fili Cothurnes',
+	[27430] = {Scherzo=0.1},    -- 'Fili Cothurnes +1',
 	[23362] = {Scherzo=0.1},    -- 'Fili Cothurnes +2',
 	[23697] = {Scherzo=0.1},    -- 'Fili Cothurnes +3',
-    [26255] = {Madrigal=0.1,Prelude=0.1}, -- 'Intarabus\'s Cape',
-    [25561] = {Etude=0.1},      -- 'Mousai Turban',
-    [25562] = {Etude=0.2},      -- 'Mousai Turban +1',
+	[26255] = {Madrigal=0.1,Prelude=0.1}, -- 'Intarabus\'s Cape',
+	[25561] = {Etude=0.1},      -- 'Mousai Turban',
+	[25562] = {Etude=0.2},      -- 'Mousai Turban +1',
 	[26537] = {Threnody=0.1},	-- 'Mousai Manteel',
 	[26538] = {Threnody=0.2},	-- 'Mousai Manteel +1',
-    [25988] = {Carol=0.1},      -- 'Mousai Gages',
-    [25989] = {Carol=0.2},      -- 'Mousai Gages +1',
-    [25901] = {Minne=0.1},      -- 'Mousai Seraweels',
-    [25902] = {Minne=0.2},      -- 'Mousai Seraweels +1',
-    [25968] = {Mambo=0.1},      -- 'Mousai Crackows',
-    [25969] = {Mambo=0.2},      -- 'Mousai Crackows +1',
+	[25988] = {Carol=0.1},      -- 'Mousai Gages',
+	[25989] = {Carol=0.2},      -- 'Mousai Gages +1',
+	[25901] = {Minne=0.1},      -- 'Mousai Seraweels',
+	[25902] = {Minne=0.2},      -- 'Mousai Seraweels +1',
+	[25968] = {Mambo=0.1},      -- 'Mousai Crackows',
+	[25969] = {Mambo=0.2},      -- 'Mousai Crackows +1',
     }
 
 local slots = {'main','sub','range','head','neck','body','hands','legs','feet','back'}
